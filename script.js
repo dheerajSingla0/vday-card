4541496aaada5bf21fcb6aac3224b7446df653df
dheerajSingla0 <57399197+dheerajSingla0@users.noreply.github.com>
2026-02-01 22:53:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const noBtn = document.getElementById("no");
+
+noBtn.addEventListener("mouseenter", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 100 - 50;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
