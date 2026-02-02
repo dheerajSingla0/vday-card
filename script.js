772c4effc74de87bcce7c95d5f472696e8ec9556
dheerajSingla0 <57399197+dheerajSingla0@users.noreply.github.com>
2026-02-02 14:16:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// Page switch
+document.getElementById("startBtn").addEventListener("click", goToPage2);
+
 function goToPage2() {
   document.getElementById("page1").classList.add("hidden");
   document.getElementById("page2").classList.remove("hidden");
 }
 
-// existing code
+// Moving NO button
 const noBtn = document.getElementById("no");
 
 noBtn.addEventListener("mouseenter", () => {
-  const x = Math.random() * 200 - 100;
-  const y = Math.random() * 100 - 50;
+  const x = Math.random() * 160 - 80;
+  const y = Math.random() * 80 - 40;
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
